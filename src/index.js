61bf918ea2c5e17dfeb4ee58d7fb5d491d3ce2c9
Srinivas nalla <nallasrinivas678@outlook.com>
2022-07-08 22:53:34 -0400

    initial commit - ReactAuth0Demo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain='dev-6mmwh-2h.us.auth0.com'
+    clientId='bvR9wsm832EZQeWr6SMEFh2JmylBGx6a'
+    redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
